fcf7266fd79c730c4c5c9964198446615836a893
Anitha-P <anitha95anitha@gmail.com>
2016-07-15 06:03:39 +0530

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%d",&c);
+if(c=='A'||c=='O'||c=='I'||c=='E'||c=='U'||c=='a'||c=='o'||c=='i'||c=='e'||c=='u')
+printf("vowels");
+else printf("consonants");
+getch();
+}
