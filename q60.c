a12877ac0ba3ea962adbab140ab848faebd15862
Anitha-P <anitha95anitha@gmail.com>
2016-07-17 20:57:15 +0530

    Create q60.c

---
+++ q60.c
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Rombus
+{
+	public static void main (String[] args) 
+	{
+		
+		String s="hello world";
+		StringTokenizer t=new StringTokenizer(s," ");
+		System.out.println(t.countTokens());
+	}
+}
